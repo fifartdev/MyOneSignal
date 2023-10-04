d96a731e8630f3e1638c2e59b1bb9394944cc025
fifartdev <fifartapp@gmail.com>
2023-10-04 16:09:00 +0300

    repo init

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { LogLevel, OneSignal } from "react-native-onesignal";
+
+OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+OneSignal.initialize("18b2f697-c496-4cf4-bf00-1085bf562bc5");
+
+// Also need enable notifications to complete OneSignal setup
+OneSignal.Notifications.requestPermission(true);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello from MyOneSignal App</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
